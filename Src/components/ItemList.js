64c36782cdc1eadcd4a968cadbea83cde8ff0c7a
Naman <namankumar62039@gmail.com>
2023-09-24 13:02:32 +0530

    made some changes and made one new component

--- Src/components/ItemList.js
+++ Src/components/ItemList.js
@@ -1,8 +1,28 @@
-const ItemList = ({items}) => {
+const ItemList = ({ items }) => {
     console.log(items)
     return (
         <div>
-            itemsList
+            {items.map((items) => (<div key={items.card.info.id} className="p-2 m-2 border-b-2 text-left flex justify-between">
+
+
+                <div>
+                    <div className="mb-4">
+                        <span className="font-semibold">{items.card.info.name}</span>
+                        <span className="font-normal">  -  ₹{items.card.info.price / 100}</span>
+                    </div>
+                    <div className="w-96">
+                        <p className="text-xs">{items.card.info.description}</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="absolute">
+                        <button className="p-1 m-auto rounded-md shadow-lg bg-black text-white">ADD+</button>
+                    </div>
+                    <img className="w-32" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + items.card.info.imageId} />
+                </div>
+
+            </div>))}
         </div>
     );
 };
--- Src/components/ResCatogery.js
+++ Src/components/ResCatogery.js
@@ -5,7 +5,7 @@ const ResCatogery = ({ Data }) => {
         <div className="m-4">
             <div className="w-6/12 m-auto bg-slate-100 p-4 items-center rounded-md shadow-md hover:bg-slate-200">
                     <div className=" flex justify-between">
-                        <span className="font-medium">{Data.title} ({Data.itemCards.length})</span>
+                        <span className="font-bold">{Data.title} ({Data.itemCards.length})</span>
                         <span>🔻</span>
                     </div>
                 <ItemList items={Data.itemCards} />
