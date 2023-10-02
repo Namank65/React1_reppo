ad16f3f882e750526343a05b752b9866bdfc0c9d
Naman <namankumar62039@gmail.com>
2023-10-03 00:26:21 +0530

    writing test cases

--- Src/components/Contact.js
+++ Src/components/Contact.js
@@ -1,10 +1,12 @@
 const Contact = () => {
     return (
-        <div>
-            <h1>Its a contact page</h1>
-            <h2>This page is created just for testing purposes</h2>
+        <div className="text-center m-4 p-4">
+            <h1 className="font-bold text-xl m-4 p-4">Contact Us</h1>
+            <input className="border border-black rounded-lg m-2 p-2" placeholder="Name"></input>
+            <input className="border border-black rounded-lg m-2 p-2" placeholder="Your Message"></input>
+            <button className="m-2 p-2 bg-green-300 rounded-lg">Submit</button>
         </div>
-    )
+    );
 };
 
 export default Contact;
