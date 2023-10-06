5f2a7081d4dda17428cd6f13066abc6e93703578
Naman <namankumar62039@gmail.com>
2023-10-07 01:03:46 +0530

    learning unit  testing

--- Src/components/__tests__/ResCards.test.js
+++ Src/components/__tests__/ResCards.test.js
@@ -1,16 +1,10 @@
 import {render, screen} from "@testing-library/react";
 import ResCards from "../ResCards";
-import {withIsOpenLable} from "../ResCards"
-import MOCK_DATA from "../mocks/ResCardMock";
+import {MOCK_DATA} from "../mocks/ResCardMock";
 import "@testing-library/jest-dom";
 
 it("Should render ResCard component with prop data", () => {
     render(<ResCards resData={MOCK_DATA}/>)
-    const name = screen.getByText("Oven Story Pizza - Standout Toppings");
+    const name = screen.getByText("Sujata Family Restaurant");
     expect(name).toBeInTheDocument()
-})
-it("Should render ResCard component with prop data", () => {
-    render(withIsOpenLable())
-    const name = screen.getByText("Oven Story Pizza - Standout Toppings");
-    expect(name).toBeInTheDocument()
-})
+});
