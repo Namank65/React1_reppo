a718fa892ba269ff570bbb9c24eca84446c894e6
Naman <namankumar62039@gmail.com>
2023-10-13 00:52:33 +0530

    initial commit

--- Src/components/ResCards.js
+++ Src/components/ResCards.js
@@ -6,7 +6,7 @@ const ResCards = (props) => {
       <div data-testid="resCard" className="m-4 p-4 w-[250px] h-[500px] bg-slate-300 rounded-lg break-words hover:bg-slate-400">
         <img className="rounded-lg h-[250px] w-[250px] " src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId} />
         <h3 className="font-bold py-4 text-lg">{name}</h3>
-        <h4 className="">{cuisines.join(",")}</h4>
+        <h4>{cuisines.join(",")}</h4>
         <h4>{avgRating} Stars</h4>
         <h4>{costForTwo}</h4>
         <h4>Delivery Time is {resData.info.sla.deliveryTime} minutes</h4>
